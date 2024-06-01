139ad645f91eb262106a6e45fe3637a964d6a4cc
Lucas Scquiavon <118318155+LucasFnavarro@users.noreply.github.com>
2024-06-01 12:36:52 -0300

    Commit

---
+++ Aula01-Rest-Na-Pratica/Aula01-Rest-Na-Pratica/Controllers/CalculatorController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula01_Rest_Na_Pratica.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+     
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get( string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input.");
+        }
+        private bool IsNumeric(string strNumber)
+        {
+
+            double number;
+            bool IsNumeric = double.TryParse(
+                strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out number);
+
+            return IsNumeric;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+    }
+}
